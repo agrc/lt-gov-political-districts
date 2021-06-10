8e9c92e174efe70b4b16c01b43db1714da11f293
stdavis <stdavis@utah.gov>
2021-06-10 11:56:09 -0600

    fix: fix baseUrl for relative and remove extra var

--- src/js/agrc_map.js
+++ src/js/agrc_map.js
@@ -1,25 +1,24 @@
 (function () {
     // dev
-    // var base = baseUrl = 'https://politicaldistricts.dev.utah.gov/';
+    // var baseUrl = 'https://politicaldistricts.dev.utah.gov/';
 
     // prod
-    // var base = baseUrl = 'https://politicaldistricts.agrc.utah.gov/';
+    // var baseUrl = 'https://politicaldistricts.agrc.utah.gov/';
 
     // same host, use relative
-    var base = '';
-    var baseUrl = './dojo/';
+    var baseUrl = './';
 
     var head = document.getElementsByTagName('head').item(0);
 
     var link = document.createElement('link');
     link.rel = 'stylesheet';
     link.type = 'text/css';
-    link.href = base + 'app/css/App.css';
+    link.href = baseUrl + 'app/css/App.css';
     head.appendChild(link);
 
     var script = document.createElement('script');
     script.type = 'text/javascript';
-    script.src = base + 'dojo/dojo.js';
+    script.src = baseUrl + 'dojo/dojo.js';
     script.setAttribute('data-dojo-config', 'deps: ["app/run"], baseUrl: "' + baseUrl + '"');
     head.appendChild(script);
 }());
